3b243d2df833261eff7938e7847f199a39432c61
Mahdi <m2hdtl@gmail.com>
2021-09-24 00:47:15 +0330

    router class's structure improved

---
+++ Core/Middleware/Authentication.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Zed\Framework\Middleware;
+
+use Zed\Framework\Exception\MiddlewareException;
+use Zed\Framework\{Response, Token};
+
+/**
+ * @author @SMhdHsn
+ * 
+ * @version 1.0.1
+ */
+trait Authentication
+{
+    /**
+     * Checking User's Authentication Status.
+     * 
+     * @since 1.0.1
+     * 
+     * @return void
+     */
+    private static function auth(): void
+    {
+        if (! (new Token)->verify())
+            throw new MiddlewareException(
+                Response::UNAUTHORIZED, 
+                Response::HTTP_UNAUTHORIZED
+            );
+    }
+}
